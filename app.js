7dbaeaee798c553a1ba9252c01f85fa6a1e1aa46
ashabrah <ashabrah@indiana.edu>
2016-08-11 22:52:58 -0400

    fga commit

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 var express = require('express');
 var MongoClient = require('mongodb');
-var monk =require('monk');
 var app = express();
 var mongoose = require('mongoose');
-var uri = 'mongodb://samr:Reb1992ecca@ds153735.mlab.com:53735/fga' 
-//var db = monk(uri);
+var uri = 'mongodb://heroku_npcx43xq:pavdea4s5oe0t1go3q6h35ogcq@ds153715.mlab.com:53715/heroku_npcx43xq' 
 
-var Db = require('mongodb').Db;
-var Server = require('mongodb').Server;
+
+var db = mongoose.connection;
+
 
 app.get('/', function(req, res, next){
 	
-	var db = new Db('fga', new Server('ds153735.mlab.com', 53735));
-
-	//var collection = db.get('students');
-	//console.log(db);
-	/*collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
-        });
-		console.log("ddfdf"+e);
-    });*/
-	db.open(function(err, db) {
-		console.log("open");
-		cursor = db.collection('students').find();
-		cursor.toArray(function(err, documents) {
-			console.log(documents);
-		});
+	db.on('error', console.error);
+	db.once('open', function(){
+		console.log("vandacchu");
+	});
+	mongoose.connect(uri);
+	var studentSchema = new mongoose.Schema({
+ 						 firstName: String 
+						, lastName: String
+						, email: String
+						, address: String
+						, phone: Number
+					});
+	var Student = mongoose.model('student', studentSchema);
+	Student.find({}, function(err, students) {
+  		if (err) return console.error(err);
+  		console.log(JSON.stringify(students));
 	});
 
 });
